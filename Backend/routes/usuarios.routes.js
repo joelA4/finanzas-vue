007a1c5d66410a06226e622ceca4692c754e4020
joelA4 <joelalbertohe23@gmail.com>
2025-06-11 22:50:18 -0500

    Actualizar rutas (obtenerPerfil y actualizarPerfil) y agregar autenticacion

--- Backend/routes/usuarios.routes.js
+++ Backend/routes/usuarios.routes.js
@@ -1,8 +1,19 @@
 import express from 'express'
-import { obtenerUsuarios } from '../controllers/usuarios.controller.js'
+import { 
+    obtenerUsuarios,
+    obtenerPerfil,
+    actualizarPerfil
+} from '../controllers/usuarios.controller.js'
+import { autenticar } from '../middleawares/auth.js'
 
 const router = express.Router()
 
-router.get('/', obtenerUsuarios)
+//Todas las rutas requieren autenticacion
+router.use(autenticar)
+
+//Rutas de usuario
+router.get('/', obtenerUsuarios) //Solo admin
+router.get('/perfil', obtenerPerfil)
+router.put('/perfil', actualizarPerfil)
 
 export default router
